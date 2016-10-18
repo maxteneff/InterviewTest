d9ec2b0d6209c98f7704ea98d32b19eb9e308448
Max Bartenev <max.bartenev@yandex.ru>
2016-10-18 15:13:59 +0300

    added test of processing input records

--- build.sbt
+++ build.sbt
@@ -4,6 +4,8 @@ version := "1.0"
 
 scalaVersion := "2.11.8"
 
+parallelExecution in Test := false
+
 libraryDependencies ++= Seq(
   "org.apache.spark" %% "spark-core" % "1.6.0" % "provided",
   "org.apache.spark" %% "spark-streaming" % "1.6.0" % "provided",
--- src/test/scala/StreamingTest.scala
+++ src/test/scala/StreamingTest.scala
@@ -1,20 +1,42 @@
 import com.holdenkarau.spark.testing.StreamingSuiteBase
-import org.apache.spark.streaming.dstream.DStream
+import org.apache.spark.streaming.{Duration, Seconds}
 
 /**
   * Created by user on 18.10.16.
   */
 class StreamingTest extends StreamingSuiteBase {
 
-  test("") {
-    val input = List(List("hi"), List("hi holden"), List("bye"))
-    val expected = List(List("hi"), List("hi", "holden"), List("bye"))
-    testOperation[String, String](input, tokenize _, expected, ordered = false)
-  }
+  // Batch duration
+  override def batchDuration: Duration = Seconds(1)
 
-  // This is the sample operation we are testing
-  def tokenize(f: DStream[String]): DStream[String] = {
-    f.flatMap(_.split(" "))
+  // Whether to use manual clock or not
+  override def useManualClock: Boolean = true
+
+  // Whether to actually wait in real time before changing manual clock
+//  override def actuallyWait: Boolean = true
+
+  test("test processing input records") {
+    //OK batch
+    val inputBatch1 = List( "host-1 81.73.150.239 2013-10-10T04:02:21.000 \"/handle-bars\" 101",
+                            "host-2 123.221.14.56 2013-10-10T03:14:19.000 \"/forks\" 200",
+                            "host-3 233.192.62.103 2013-10-10T03:18:25.000 \"/store\" 400")
+    //wrong input
+    val inputBatch2 = List("Wrong input")
+    // empty batch
+    val inputBatch3 = null
+    // wrong (non-int) code
+    val inputBatch4 = List( "host-1 81.73.150.239 2013-10-10T04:02:21.000 \"/handle-bars\" ab200") //wrong code
+    val input = List(inputBatch1, inputBatch2, inputBatch3, inputBatch4)
+
+    val exBatch1 = List(LogRecord("host-1", "81.73.150.239", "2013-10-10T04:02:21.000", "\"/handle-bars\"", 101),
+                        LogRecord("host-2", "123.221.14.56", "2013-10-10T03:14:19.000", "\"/forks\"", 200),
+                        LogRecord("host-3", "233.192.62.103", "2013-10-10T03:18:25.000", "\"/store\"", 400))
+    //wrong input
+    val exBatch2 = List()
+    // wrong (non-int) code
+    val exBatch3 = List()
+    val expected = List(exBatch1, exBatch2, exBatch3)
+    testOperation[String, LogRecord](input,  ServerLogsStreaming.createRecordStream _, expected, ordered = true)
   }
 
 }
